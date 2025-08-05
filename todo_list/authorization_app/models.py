7fd9a0d65072ebbb67cc1d1403353f47c39573e6
nikitsss303 <nikita.a.smantser@gmail.com>
2025-08-05 15:53:47 +0300

    Implement view for creating tasks

--- todo_list/authorization_app/models.py
+++ todo_list/authorization_app/models.py
@@ -3,11 +3,11 @@ from django.conf import settings
 
 # Create your models here.
 class Todo(models.Model):
-    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     title = models.CharField(max_length=50)
     description = models.TextField(blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     is_completed = models.BooleanField(default=False)
+    user_id = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.title
--- todo_list/list_app/views.py
+++ todo_list/list_app/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, redirect
+from authorization_app.models import Todo 
 
 # Create your views here.
 def list_view(request):
@@ -6,4 +7,18 @@ def list_view(request):
 
 
 def create_view(request):
+    if request.method == 'POST':
+        action = request.POST.get('action')
+
+        title = request.POST.get('title')
+        description = request.POST.get('description')
+        user_id = request.user
+
+        todo = Todo.objects.create(title=title, description=description, user_id=user_id)
+        todo.save()
+        
+        if action == 'create':
+            return render(request, 'create.html')
+        else:
+            return redirect('list_app:list')
     return render(request, 'create.html')
